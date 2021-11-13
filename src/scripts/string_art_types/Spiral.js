7fe6fea1f74924eec2c5ffff0821c778b6089f24
Yossi Kolesnicov <yoxigen@gmail.com>
2021-11-14 00:10:01 +0200

    Multicolor spiral

--- src/scripts/string_art_types/Spiral.js
+++ src/scripts/string_art_types/Spiral.js
@@ -2,6 +2,31 @@ import Color from "../helpers/Color.js";
 import StringArt from "../StringArt.js";
 import Circle from "./Circle.js";
 
+const COLOR_CONFIG = Color.getConfig({ 
+    defaults: {
+        isMultiColor: true,
+        color: "#ffbb29",
+        multicolorRange: "21",
+        multicolorStart: 32,
+        multicolorByLightness: true,
+        minLightness: 36,
+        maxLightness: 98
+    },
+});
+
+COLOR_CONFIG.children.splice(COLOR_CONFIG.children.findIndex(({key}) => key === 'isMultiColor'), 0, {
+    key: 'colorCount',
+    label: 'Colors count',
+    defaultValue: 7,
+    type: "range",
+    attr: {
+        min: 1,
+        max: 20,
+        step: 1,
+    },
+    show: ({isMultiColor}) => isMultiColor
+});
+
 export default class Spiral extends StringArt{
     id = "spiral";
     name = "Spiral";
@@ -38,18 +63,12 @@ export default class Spiral extends StringArt{
             ...Circle.rotationConfig,
             defaultValue: 0.75,
         },
-        Color.getConfig({ 
-            defaults: {
-                isMultiColor: false,
-                color: "#00ff7b",
-            },
-            include: ["color"]
-        }),
+        COLOR_CONFIG,
     ];
 
     setUpDraw() {
         super.setUpDraw();
-        const { n, rotation, layers, margin } = this.config;
+        const { n, rotation, layers, margin, colorCount } = this.config;
         this.layersCount = layers ?? 1;
 
         this.circle = new Circle({
@@ -61,12 +80,21 @@ export default class Spiral extends StringArt{
 
         this.color = new Color({
             ...this.config,
-            colorCount: this.layersCount
+            colorCount: layers ?? colorCount
         });
+
+        if (colorCount) {
+            const stepCount = this.getStepCount();
+            const stepsPerColor = Math.floor(stepCount / colorCount);
+            this.colorsMap = new Map();
+            for (let i = 0; i < colorCount; i++) {
+                this.colorsMap.set(i * stepsPerColor, this.color.getColor(i))
+            }
+        }
     }
 
     *drawSpiral({ shift = 0, color = "#ffffff" } = {}) {
-        const {repetition, innerLength, n} = this.config;
+        const {repetition, innerLength, n, colorCount} = this.config;
 
         let currentInnerLength = Math.round(innerLength * n);
         let repetitionCount = 0;
@@ -75,6 +103,13 @@ export default class Spiral extends StringArt{
         let isPrevPoint = false;
 
         for(let i=0; currentInnerLength > 0; i++) {
+            if (this.colorsMap) {
+                const stepColor = this.colorsMap.get(i);
+                if (stepColor) {
+                    this.ctx.strokeStyle = stepColor;
+                }
+            }
+            
             this.ctx.beginPath();
             this.ctx.moveTo(...prevPoint);
             const nextPointIndex = isPrevPoint ? i + shift : i + currentInnerLength + shift;
