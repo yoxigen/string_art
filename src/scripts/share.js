5c4f0a481ef210a185d3a33b7f41cdba3f2ec7e7
Yossi Kolesnicov <yoxigen@gmail.com>
2021-12-22 22:31:10 +0200

    Fixing share

--- src/scripts/share.js
+++ src/scripts/share.js
@@ -1,32 +1,32 @@
 export async function share(input) {
+  try {
     navigator.share(await getShareData(input));
+  } catch (error) {
+    alert('Error: ' + error.message);
+  }
 }
 
 export async function isShareSupported(input) {
-    if (!navigator.share) {
-        return false;
-    }
+  if (!navigator.share) {
+    return false;
+  }
 
-    const shareData = await getShareData(input);
-    return navigator.canShare(shareData);
+  const shareData = await getShareData(input);
+  return navigator.canShare(shareData);
 }
 
-async function getShareData({canvas, pattern}) {
-    const dataUrl = canvas.toDataURL();
-    const blob = await (await fetch(dataUrl)).blob();
-    const files = [
-        new File(
-            [blob],
-            pattern.name,
-            {
-                type: blob.type,
-                lastModified: new Date().getTime()
-            }
-        )
-    ];
-    return {
-        url: window.location.href,
-        files,
-        title: document.title
-    };
+async function getShareData({ canvas, pattern }) {
+  const dataUrl = canvas.toDataURL();
+  const blob = await (await fetch(dataUrl)).blob();
+  const files = [
+    new File([blob], pattern.name + '.jpg', {
+      type: blob.type,
+      lastModified: new Date().getTime(),
+    }),
+  ];
+  return {
+    url: window.location.href,
+    files,
+    title: document.title,
+  };
 }
