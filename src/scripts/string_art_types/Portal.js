8559ecc45e0181083b27169c2c3b7fd804162a00
Yossi Kolesnicov <yoxigen@gmail.com>
2022-12-27 08:08:26 +0200

    progress with Portal

--- src/scripts/string_art_types/Portal.js
+++ src/scripts/string_art_types/Portal.js
@@ -41,106 +41,148 @@ export default class Portal extends StringArt {
       isStructural: true,
     },
     {
-        key: 'circleX',
-        label: 'Circle X',
-        defaultValue: 0.5,
-        type: 'range',
-        attr: {
-          min: 0,
-          max: 1,
-          step: 0.01,
-        },
-        displayValue: ({ circleX }) => Math.round(circleX * 100) + '%',
-        isStructural: true,
+      key: 'circleX',
+      label: 'Circle X',
+      defaultValue: 0.5,
+      type: 'range',
+      attr: {
+        min: 0,
+        max: 1,
+        step: 0.01,
       },
-      {
-        key: 'circleY',
-        label: 'Circle Y',
-        defaultValue: 0.5,
-        type: 'range',
-        attr: {
-          min: 0,
-          max: 1,
-          step: 0.01,
-        },
-        displayValue: ({ circleY }) => Math.round(circleY * 100) + '%',
-        isStructural: true,
+      displayValue: ({ circleX }) => Math.round(circleX * 100) + '%',
+      isStructural: true,
+    },
+    {
+      key: 'circleY',
+      label: 'Circle Y',
+      defaultValue: 0.5,
+      type: 'range',
+      attr: {
+        min: 0,
+        max: 1,
+        step: 0.01,
       },
+      displayValue: ({ circleY }) => Math.round(circleY * 100) + '%',
+      isStructural: true,
+    },
   ];
 
   getCalc() {
-    const {density, r, circleX, circleY} = this.config;
+    const { density, r, circleX, circleY } = this.config;
     const size = this.size ?? this.getSize();
     const smallSide = Math.min(...size);
-    const radius = smallSide * r / 100;
+    const radius = (smallSide * r) / 100;
     const diameter = radius * 2;
-    const circleCenter = [radius + (size[0] - diameter) * circleX, radius + (size[1] - diameter) * circleY];
+    const circleCenter = [
+      radius + (size[0] - diameter) * circleX,
+      radius + (size[1] - diameter) * circleY,
+    ];
+    const nailDistance = Math.max(...size) / density;
+    const countWidth = Math.round(size[0] / nailDistance);
+    const countHeight = Math.round(size[1] / nailDistance);
+
+    const sides = [
+      { count: countWidth, getPoint: i => [i * nailDistance, 0] },
+      { count: countHeight, getPoint: i => [width, i * nailDistance] },
+      { count: countWidth, getPoint: i => [width - i * nailDistance, height] },
+      { count: countHeight, getPoint: i => [0, height - i * nailDistance] },
+    ];
 
     return {
-        nailDistance: Math.max(...size) / density,
-        radius,
-        circleCenter,
-        ranges: this.getRanges({ size, radius, circleCenter })
+      nailDistance,
+      radius,
+      circleCenter,
+      sides,
+      ...this.getRanges({ size, radius, circleCenter }),
     };
+  }
+
+  getPointDistanceToCircleCenter(point) {
+    return Math.sqrt(
+      (point[0] - this.calc.circleCenter[0]) ** 2 +
+        (point[1] - this.calc.circleCenter[1]) ** 2
+    );
   }
 
   getRanges({ size, radius, circleCenter }) {
     const ranges = [];
 
-    // Bottom of right side
-    const bottomRightDistanceFromCircleCenter = Math.sqrt((size[0] - circleCenter[0]) ** 2 + (size[1] - circleCenter[1]) ** 2);
-    const angleToTopSide = Math.asin(radius / bottomRightDistanceFromCircleCenter) + Math.atan((size[0] - circleCenter[0]) / (size[1] - circleCenter[1]));
-    const topSideX = size[0] - size[1] * Math.tan(angleToTopSide);
-
-    if (topSideX >= 0) {
-        ranges.rightBottom = [topSideX, 0];
-    } else {
-        const leftSideY = size[1] - size[0] / Math.tan(angleToTopSide);
-        ranges.rightBottom = [0, leftSideY];
-    }
-
-    // Right of top side
-    const rightTopDistanceFromCircleCenter = Math.sqrt(circleCenter[1] ** 2 + (size[0] - circleCenter[0]) ** 2);
-    const angleToLeftSide = Math.atan(circleCenter[1] / (size[0] - circleCenter[0])) + Math.asin(radius / rightTopDistanceFromCircleCenter);
-    const bottomSideXFromRightTop = size[0] - size[1] / Math.tan(angleToLeftSide);
-
-    if (bottomSideXFromRightTop >= 0) {
-        ranges.rightTop = [bottomSideXFromRightTop, size[1]];
-    } else {
-        const leftSideY = size[0] * Math.tan(angleToLeftSide);
-        ranges.rightTop = [0, leftSideY];
-    }
+    let startAtRightSide = true;
 
     // Top of left side
-    const leftTopDistanceFromCircleCenter = Math.sqrt(circleCenter[0] ** 2 + circleCenter[1] ** 2);
-    const angleToRightSide = Math.PI / 2 - (Math.asin(radius / leftTopDistanceFromCircleCenter) + Math.atan(circleCenter[0] / circleCenter[1]));
+    const leftTopDistanceFromCircleCenter = this.getPointDistanceToCircleCenter(
+      [0, 0]
+    );
+    const angleToRightSide =
+      Math.PI / 2 -
+      (Math.asin(radius / leftTopDistanceFromCircleCenter) +
+        Math.atan(circleCenter[0] / circleCenter[1]));
     const rightSideY = size[0] * Math.tan(angleToRightSide);
 
     if (rightSideY <= size[1]) {
-        ranges.leftTop = [size[0], rightSideY];
+      ranges.leftTop = [size[0], rightSideY];
     } else {
-        const bottomSideX = size[1] / Math.tan(angleToRightSide);
-        ranges.leftTop = [bottomSideX, size[1]];
+      startAtRightSide = false;
+      const bottomSideX = size[1] / Math.tan(angleToRightSide);
+      ranges.leftTop = [bottomSideX, size[1]];
+    }
+
+    // Right of top side
+    const rightTopDistanceFromCircleCenter =
+      this.getPointDistanceToCircleCenter([size[0], 0]);
+    const angleToLeftSide =
+      Math.atan(circleCenter[1] / (size[0] - circleCenter[0])) +
+      Math.asin(radius / rightTopDistanceFromCircleCenter);
+    const bottomSideXFromRightTop =
+      size[0] - size[1] / Math.tan(angleToLeftSide);
+
+    if (bottomSideXFromRightTop >= 0) {
+      ranges.rightTop = [bottomSideXFromRightTop, size[1]];
+    } else {
+      const leftSideY = size[0] * Math.tan(angleToLeftSide);
+      ranges.rightTop = [0, leftSideY];
+    }
+
+    // Bottom of right side
+    const bottomRightDistanceFromCircleCenter =
+      this.getPointDistanceToCircleCenter(size);
+    const angleToTopSide =
+      Math.asin(radius / bottomRightDistanceFromCircleCenter) +
+      Math.atan((size[0] - circleCenter[0]) / (size[1] - circleCenter[1]));
+    const topSideX = size[0] - size[1] * Math.tan(angleToTopSide);
+
+    if (topSideX >= 0) {
+      ranges.rightBottom = [topSideX, 0];
+    } else {
+      const leftSideY = size[1] - size[0] / Math.tan(angleToTopSide);
+      ranges.rightBottom = [0, leftSideY];
     }
 
     // Left of bottom side
-    const leftBottomDistanceFromCircleCenter = Math.sqrt(circleCenter[0] ** 2 + (size[1] - circleCenter[1]) ** 2);
-    const angleFromTopSide = Math.atan((size[1] - circleCenter[1]) / circleCenter[0]) + Math.asin(radius / leftBottomDistanceFromCircleCenter);
+    const leftBottomDistanceFromCircleCenter =
+      this.getPointDistanceToCircleCenter([0, size[1]]);
+    const angleFromTopSide =
+      Math.atan((size[1] - circleCenter[1]) / circleCenter[0]) +
+      Math.asin(radius / leftBottomDistanceFromCircleCenter);
     const topSideXFromBottomLeft = size[1] / Math.tan(angleFromTopSide);
 
     if (topSideXFromBottomLeft <= size[0]) {
-        ranges.leftBottom = [topSideXFromBottomLeft, 0];
+      ranges.leftBottom = [topSideXFromBottomLeft, 0];
     } else {
-        const rightSideY = size[1] - size[0] * Math.tan(angleFromTopSide);
-        ranges.leftBottom = [size[0], rightSideY];
+      const rightSideY = size[1] - size[0] * Math.tan(angleFromTopSide);
+      ranges.leftBottom = [size[0], rightSideY];
     }
 
-    return Object.freeze([
+    return Object.freeze({
+      ranges: [
         [ranges.leftTop, [0, 0]],
         [ranges.rightTop, [size[0], 0]],
         [ranges.rightBottom, [size[0], size[1]]],
         [ranges.leftBottom, [0, size[1]]],
-    ]);
+      ],
+      firstSide: startAtRightSide ? 1 : 2,
+    });
   }
 
   resetStructure() {
@@ -165,54 +207,73 @@ export default class Portal extends StringArt {
     super.setUpDraw();
 
     if (!this.calc) {
-        this.calc = this.getCalc();
+      this.calc = this.getCalc();
     }
   }
 
-  getPointDestinationSide(point) {
-    //if ()
+  getPointDestination(point) {
+    const distanceToCircleCenter = this.getPointDistanceToCircleCenter(point);
+    const isHorizontal = point[1] === 0 || point[1] === this.size[1];
+
+    const angleBetweenPointSideAndDistanceToCircleCenter = (isHorizontal ? Math.asin(point[1] - this.calc.circleCenter[1]) : // Horizontal sinus goes here!) / distanceToCircleCenter;
+  }
+
+  getFirstPointDestinationSide() {
+    for (const range of this.calc.ranges) {
+    }
   }
 
   *generateStrings() {
-    const { circleCenter, radius} = this.calc;
+    const { circleCenter, radius } = this.calc;
 
     const points = this.generatePoints();
 
     let index = 0;
-    this.ctx.strokeStyle = "#fff";
-console.log("RAGE", this.calc.ranges)
-    for(const [source, destination] of this.calc.ranges) {
+    this.ctx.strokeStyle = '#fff';
 
-        this.ctx.beginPath();
-        this.ctx.moveTo(...source);
-        this.ctx.lineTo(...destination);
-        this.ctx.stroke();
-        yield;
-    }
-
-    // for (const point of points) {
-    //     const width = circleCenter[0] - point[0];
-    //     const height = point[1] - circleCenter[1];
-
-    //     const beta = Math.atan(height / width);
-    //     const alpha = Math.atan(height / width) + Math.asin(radius * Math.sin(beta) / height);
-
-    //     let destination = [this.size[0], point[1] - this.size[0] * Math.tan(alpha)];
-    //     if (destination[1] < 0) {
-    //         destination = [point[1] / Math.tan(alpha) + point[0], 0];
-    //     } else if (destination[1] > this.size[1]) {
-    //         destination = [(this.size[1] - point[1]) / Math.tan(Math.PI * 2 - alpha) + point[0], this.size[1]];
-    //     }
-
-    //     console.log(JSON.stringify({ point, destination, size: this.size}));
-
-    //     this.ctx.beginPath();
-    //     this.ctx.moveTo(...point);
-    //     this.ctx.lineTo(...destination);
-    //     this.ctx.stroke();
-
-    //     yield;
+    // for (const [source, destination] of this.calc.ranges) {
+    //   this.ctx.beginPath();
+    //   this.ctx.moveTo(...source);
+    //   this.ctx.lineTo(...destination);
+    //   this.ctx.stroke();
+    //   yield;
     // }
+
+    let sideIndex = this.calc.firstSide;
+    let side = this.calc.sides[sideIndex];
+    let range = this.calc.ranges[0][0];
+
+    for (const point of points) {
+      const width = circleCenter[0] - point[0];
+      const height = point[1] - circleCenter[1];
+
+      const beta = Math.atan(height / width);
+      const alpha =
+        Math.atan(height / width) +
+        Math.asin((radius * Math.sin(beta)) / height);
+
+      let destination = [
+        this.size[0],
+        point[1] - this.size[0] * Math.tan(alpha),
+      ];
+      if (destination[1] < 0) {
+        destination = [point[1] / Math.tan(alpha) + point[0], 0];
+      } else if (destination[1] > this.size[1]) {
+        destination = [
+          (this.size[1] - point[1]) / Math.tan(Math.PI * 2 - alpha) + point[0],
+          this.size[1],
+        ];
+      }
+
+      console.log(JSON.stringify({ point, destination, size: this.size }));
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(...point);
+      this.ctx.lineTo(...destination);
+      this.ctx.stroke();
+
+      yield;
+    }
   }
 
   *generatePoints() {
@@ -223,16 +284,16 @@ console.log("RAGE", this.calc.ranges)
     const countHeight = Math.round(height / nailDistance);
 
     const sides = [
-        { count: countWidth, getPoint: i => [i * nailDistance, 0] },
-        { count: countHeight, getPoint: i => [width, i * nailDistance] },
-        { count: countWidth, getPoint: i => [width - i * nailDistance, height] },
-        { count: countHeight, getPoint: i => [0, height - i * nailDistance] },
+      { count: countWidth, getPoint: i => [i * nailDistance, 0] },
+      { count: countHeight, getPoint: i => [width, i * nailDistance] },
+      { count: countWidth, getPoint: i => [width - i * nailDistance, height] },
+      { count: countHeight, getPoint: i => [0, height - i * nailDistance] },
     ];
 
-    for (const {count, getPoint} of sides) {
-        for (let i=0; i < count; i++) {
-            yield getPoint(i); // Add side, use that to determine direction in getStrings
-        }
+    for (const { count, getPoint } of sides) {
+      for (let i = 0; i < count; i++) {
+        yield getPoint(i); // Add side, use that to determine direction in getStrings
+      }
     }
   }
 
@@ -246,7 +307,7 @@ console.log("RAGE", this.calc.ranges)
     const points = this.generatePoints();
     let index = 0;
     for (const point of points) {
-        this.nails.addNail({ point, number: index++ });
+      this.nails.addNail({ point, number: index++ });
     }
   }
   static thumbnailConfig = {
