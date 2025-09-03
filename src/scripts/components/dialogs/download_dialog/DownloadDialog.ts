a8d6b99f6ea09c36bb31ecfd3f0145f663b7787f
Yossi Kolesnicov <yoxigen@gmail.com>
2025-09-03 22:29:54 +0300

    More styling for download dialog

--- src/scripts/components/dialogs/download_dialog/DownloadDialog.ts
+++ src/scripts/components/dialogs/download_dialog/DownloadDialog.ts
@@ -2,12 +2,16 @@ import * as styles from 'bundle-text:./DownloadDialog.css';
 import * as html from 'bundle-text:./DownloadDialog.html';
 import type ConfirmDialog from '../ConfirmDialog';
 import type StringArt from '../../../StringArt';
+import { STANDARD_SIZES_CM } from '../../../helpers/size_utils';
 
 const sheet = new CSSStyleSheet();
 sheet.replaceSync(String(styles));
 
 export default class DownloadDialog extends HTMLElement {
   private dialog: ConfirmDialog;
+  private elements: {
+    size: HTMLSelectElement;
+  };
 
   constructor() {
     super();
@@ -18,6 +22,31 @@ export default class DownloadDialog extends HTMLElement {
     this.shadowRoot!.innerHTML = String(html);
 
     this.dialog = shadow.querySelector('confirm-dialog')!;
+    this.elements = {
+      size: shadow.querySelector('#size'),
+    };
+
+    this.#setSizes();
+  }
+
+  #setSizes() {
+    const dpr = window.devicePixelRatio ?? 1;
+    const sizes = [
+      ...STANDARD_SIZES_CM,
+      {
+        id: 'Screen size',
+        dimensions: [window.screen.width, window.screen.height].map(v =>
+          Math.floor(v * dpr)
+        ),
+      },
+      { id: 'Custom size...', dimensions: null },
+    ];
+    this.elements.size.innerHTML = sizes
+      .map(
+        ({ id, dimensions }) =>
+          `<option value="${dimensions?.join(',') ?? 'custom'}">${id}</option>`
+      )
+      .join('\n');
   }
 
   /**
