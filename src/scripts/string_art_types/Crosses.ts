a487da19a7e61d4b6169a13f321f098390f3c418
Yossi Kolesnicov <yoxigen@gmail.com>
2025-10-08 22:51:50 +0300

    Converted crosses to use lines rather than star shapes

--- src/scripts/string_art_types/Crosses.ts
+++ src/scripts/string_art_types/Crosses.ts
@@ -1,31 +1,25 @@
 import StringArt from '../StringArt';
-import Circle from '../shapes/Circle';
 import Color from '../helpers/color/Color';
-import { ColorConfig, ColorMap } from '../helpers/color/color.types';
+import { ColorConfig } from '../helpers/color/color.types';
 import Renderer from '../renderers/Renderer';
-import {
-  ControlConfig,
-  ControlsConfig,
-  GroupValue,
-} from '../types/config.types';
+import { ControlsConfig } from '../types/config.types';
 import { CalcOptions } from '../types/stringart.types';
-import { Shape } from '../shapes/Shape';
-import { mapDimensions } from '../helpers/size_utils';
-import Polygon from '../shapes/Polygon';
 import { formatFractionAsPercent } from '../helpers/string_utils';
 import StarShape from '../shapes/StarShape';
-import { Coordinates } from '../types/general.types';
+import { Line } from '../shapes/Line';
 
 interface CrossesConfig extends ColorConfig {
   n: number;
-  centerRadius: number;
-  isVertical: boolean;
-  verticalShift?: number;
-  horizontalShift?: number;
+  orientation: 'vertical' | 'horizontal';
+  verticalGap: number;
+  horizontalGap?: number;
 }
 
 type TCalc = {
-  stars: StarShape[];
+  verticalLines: Line[];
+  horizontalLines: Line[][];
+  width: number;
+  height: number;
 };
 
 const COLOR_CONFIG = Color.getConfig({
@@ -60,42 +54,45 @@ export default class Crosses extends StringArt<CrossesConfig, TCalc> {
       isStructural: true,
     },
     {
-      key: 'centerRadius',
-      type: 'range',
-      label: 'Center radius',
-      attr: {
-        min: 0,
-        max: 1,
-        step: ({ n }) => 1 / n,
-      },
-      displayValue: ({ centerRadius }) => formatFractionAsPercent(centerRadius),
-      defaultValue: 0.5,
+      key: 'orientation',
+      type: 'select',
+      label: 'Orientation',
+      defaultValue: 'vertical',
+      options: [
+        { value: 'vertical', label: '↕ Vertical' },
+        { value: 'horizontal', label: '↔ Horizontal' },
+      ],
       affectsStepCount: false,
       isStructural: true,
     },
     {
-      key: 'isVertical',
-      type: 'checkbox',
-      label: 'Vertical',
-      defaultValue: true,
-      affectsStepCount: false,
-      isStructural: true,
-    },
-    {
-      key: 'verticalShift',
+      key: 'verticalGap',
       type: 'range',
-      label: 'Vertical shift',
+      label: 'Vertical gap',
       attr: {
         min: 0,
         max: 1,
         step: 0.01,
       },
-      displayValue: ({ verticalShift }) =>
-        formatFractionAsPercent(verticalShift),
+      displayValue: ({ verticalGap }) => formatFractionAsPercent(verticalGap),
       defaultValue: 0.5,
       affectsStepCount: false,
       isStructural: true,
-      show: ({ centerRadius }) => centerRadius > 0,
+    },
+    {
+      key: 'horizontalGap',
+      type: 'range',
+      label: 'Horizontal gap',
+      attr: {
+        min: 0,
+        max: 1,
+        step: 0.01,
+      },
+      displayValue: ({ horizontalGap }) =>
+        formatFractionAsPercent(horizontalGap),
+      defaultValue: 0.5,
+      affectsStepCount: false,
+      isStructural: true,
     },
     COLOR_CONFIG,
   ];
@@ -103,58 +100,74 @@ export default class Crosses extends StringArt<CrossesConfig, TCalc> {
   color: Color;
 
   getCalc({ size }: CalcOptions): TCalc {
-    const sizeCenter = this.center;
-    const { n, centerRadius, isVertical, margin, verticalShift } = this.config;
+    const center = this.center;
+    const {
+      n,
+      orientation,
+      margin,
+      verticalGap: verticalGapPercent,
+      horizontalGap,
+    } = this.config;
+    const isVertical = orientation === 'vertical';
     const length = (isVertical ? size[1] : size[0]) - 2 * margin;
+    const smallDimension = Math.min(...size) / 2 - 2 * margin;
+    const maxVerticalGap = (length * 0.8) / 3;
+    const verticalGap = verticalGapPercent * maxVerticalGap;
+    const lineLength = (length - 3 * verticalGap) / 4;
 
-    let radius =
-      length /
-      (4 * (1 - centerRadius * verticalShift) +
-        2 * centerRadius * (1 - verticalShift));
-
-    const stars = new Array(3).fill(null).map((_, i) => {
-      const starCenter =
-        i === 1
-          ? isVertical
-            ? sizeCenter[1]
-            : sizeCenter[0]
-          : i === 0
-          ? margin + radius * (1 - centerRadius * verticalShift)
-          : margin + length - radius * (1 - centerRadius * verticalShift);
-      return new StarShape({
-        sideNails: n,
-        sides: 4,
-        centerRadius,
-        center: isVertical
-          ? [sizeCenter[0], starCenter]
-          : [starCenter, sizeCenter[1]],
-        radius,
-        rotation: isVertical ? 2 : 1,
-        sidesCenterRadiusShift: [verticalShift, 0, verticalShift, 0],
+    const verticalLines = new Array(4).fill(null).map((_, i) => {
+      return new Line({
+        from: [center[0], margin + i * (lineLength + verticalGap)],
+        to: [center[0], margin + i * (lineLength + verticalGap) + lineLength],
+        n,
       });
     });
 
-    return { stars };
+    const horizontalStart = center[0] - verticalGap - lineLength;
+    const horizontalEnd = center[0] + verticalGap + lineLength;
+
+    const horizontalLines = new Array(3).fill(null).map((_, row) => {
+      const rowTop =
+        margin +
+        lineLength +
+        verticalGap / 2 +
+        row * (lineLength + verticalGap);
+
+      return [
+        new Line({
+          from: [horizontalStart, rowTop],
+          to: [horizontalStart + lineLength, rowTop],
+          n,
+        }),
+        new Line({
+          to: [horizontalEnd - lineLength, rowTop],
+          from: [horizontalEnd, rowTop],
+          n,
+        }),
+      ];
+    });
+    return {
+      verticalLines,
+      horizontalLines,
+      width: horizontalStart - horizontalEnd,
+      height: length,
+    };
   }
 
   getAspectRatio(): number {
-    const { isVertical } = this.config;
-    return isVertical ? 1 / 3 : 3;
+    const { width, height } = this.calc;
+    return width / height;
   }
 
-  private *connectSides(
+  private *connectLines(
     renderer: Renderer,
     {
       from,
       to,
-      fromSide,
-      toSide,
       isReverse = false,
     }: {
-      from: StarShape;
-      to: StarShape;
-      fromSide: number;
-      toSide: number;
+      from: Line;
+      to: Line;
       isReverse?: boolean;
     }
   ): Generator<void> {
@@ -162,47 +175,46 @@ export default class Crosses extends StringArt<CrossesConfig, TCalc> {
 
     for (let i = 0; i < n; i++) {
       renderer.renderLine(
-        from.getSidePoint(fromSide, isReverse ? n - 1 - i : i),
-        to.getSidePoint(toSide, n - i - 1)
+        from.getPoint(isReverse ? n - 1 - i : i),
+        to.getPoint(n - i - 1)
       );
       yield;
     }
   }
 
   *drawStrings(renderer: Renderer) {
-    const { stars } = this.calc;
+    const { verticalLines, horizontalLines } = this.calc;
     renderer.setColor('#ffffff');
+    // [verticalLineIndex, horizontalLineRowIndex, horizontalLineLeftOrRightIndex (0 left, 1 right)]
     const connections = [
-      [0, 0, 0, 1],
-      [0, 1, 0, 1],
-      [0, 0, 0, 3],
-      [0, 1, 0, 3],
-      [1, 0, 0, 3],
-      [1, 0, 0, 1],
-      [1, 1, 0, 1],
-      [1, 1, 0, 3],
-      [2, 2, 2, 1],
-      [2, 2, 2, 3],
-      [2, 1, 2, 1],
-      [2, 1, 2, 3],
-      [1, 1, 1, 2],
-      [1, 1, 3, 2],
-      [1, 2, 2, 1],
-      [1, 2, 2, 3],
-      [2, 1, 3, 0],
-      [2, 1, 1, 0],
-      [0, 1, 1, 2],
-      [0, 1, 3, 2],
+      [0, 0, 0],
+      [0, 0, 1],
+      [0, 1, 0],
+      [0, 1, 1],
+      [1, 0, 0],
+      [1, 0, 1],
+      [1, 1, 0],
+      [1, 1, 1],
+      [3, 2, 0],
+      [3, 2, 1],
+      [3, 1, 0],
+      [3, 1, 1],
+      [2, 1, 0],
+      [2, 1, 1],
+      [2, 2, 0],
+      [2, 2, 1],
+      [1, 2, 0],
+      [1, 2, 1],
+      [2, 0, 0],
+      [2, 0, 1],
     ];
 
-    for (const [from, to, fromSide, toSide] of connections) {
-      const isReverse = from === 1 && (to === 0 || to === 2);
+    for (const [verticalIndex, horizontalRow, horizontalIndex] of connections) {
+      const isReverse = horizontalRow < verticalIndex;
 
-      yield* this.connectSides(renderer, {
-        from: stars[from],
-        to: stars[to],
-        fromSide,
-        toSide,
+      yield* this.connectLines(renderer, {
+        from: verticalLines[verticalIndex],
+        to: horizontalLines[horizontalRow][horizontalIndex],
         isReverse,
       });
     }
@@ -215,12 +227,20 @@ export default class Crosses extends StringArt<CrossesConfig, TCalc> {
   }
 
   drawNails() {
-    this.calc.stars.forEach((star, i) =>
-      star.drawNails(this.nails, {
-        excludeSides: i === 0 ? [2] : i === 2 ? [0] : null,
-        getNumber: (side, sideIndex) =>
-          `${String.fromCharCode(65 + i)}_${side}_${sideIndex + 1}`,
+    this.calc.verticalLines.forEach((line, i) =>
+      line.drawNails(this.nails, {
+        getNumber: sideIndex =>
+          `${String.fromCharCode(65 + i)}_${sideIndex + 1}`,
       })
     );
+
+    this.calc.horizontalLines.forEach(([leftLine, rightLine], row) => {
+      leftLine.drawNails(this.nails, {
+        getNumber: sideIndex => `L_${row + 1}_${sideIndex}`,
+      });
+      rightLine.drawNails(this.nails, {
+        getNumber: sideIndex => `R_${row + 1}_${sideIndex}`,
+      });
+    });
   }
 }
