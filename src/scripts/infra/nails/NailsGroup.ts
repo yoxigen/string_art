1daa0e4cef7dc176c828e929bb0ab66e68c10433
Yossi Kolesnicov <yoxigen@gmail.com>
2025-10-29 13:19:33 +0200

    Using NailsGroup to render nails

---
+++ src/scripts/infra/nails/NailsGroup.ts
@@ -0,0 +1,37 @@
+import { NailsRenderOptions } from '../../types/stringart.types';
+
+export default class NailsGroup {
+  #xCoordinates: Float16Array;
+  #yCoordinates: Float16Array;
+  #numbers: Array<string | number>;
+
+  constructor(public length: number, public options: NailsRenderOptions) {
+    this.#xCoordinates = new Float16Array(length);
+    this.#yCoordinates = new Float16Array(length);
+    this.#numbers = new Array(length);
+  }
+
+  setNail(index: number, x: number, y: number, number: string | number) {
+    this.#xCoordinates[index] = x;
+    this.#yCoordinates[index] = y;
+    this.#numbers[index] = number;
+  }
+
+  forEach(
+    callback: (
+      x: number,
+      y: number,
+      number: string | number,
+      index: number
+    ) => void
+  ): void {
+    for (let i = 0; i < this.length; i++) {
+      callback(
+        this.#xCoordinates[i],
+        this.#yCoordinates[i],
+        this.#numbers[i],
+        i
+      );
+    }
+  }
+}
